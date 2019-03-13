e153883f04b214f336d6022146f51e2b287a638d
Joas Schilling <coding@schilljs.com>
2019-03-13 20:53:48 +0100

    Add a provider for linked collaboration resources
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -28,6 +28,8 @@ use OCA\Spreed\Chat\ChatManager;
 use OCA\Spreed\Chat\Command\Listener as CommandListener;
 use OCA\Spreed\Chat\Parser\Listener as ParserListener;
 use OCA\Spreed\Chat\SystemMessage\Listener as SystemMessageListener;
+use OCA\Spreed\Collaboration\Resources\ConversationProvider;
+use OCA\Spreed\Collaboration\Resources\Listener as ResourceListener;
 use OCA\Spreed\Config;
 use OCA\Spreed\Files\Listener as FilesListener;
 use OCA\Spreed\Files\TemplateLoader as FilesTemplateLoader;
@@ -44,6 +46,7 @@ use OCA\Spreed\Signaling\Listener as SignalingListener;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Http\ContentSecurityPolicy;
 use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\Collaboration\Resources\IManager as IResourceManager;
 use OCP\IServerContainer;
 use OCP\IUser;
 use OCP\Settings\IManager;
@@ -77,6 +80,7 @@ class Application extends App {
 
 		$this->extendDefaultContentSecurityPolicy();
 		$this->registerNotifier($server);
+		$this->registerCollaborationResourceProvider($server);
 		$this->getContainer()->registerCapability(Capabilities::class);
 
 		$dispatcher = $server->getEventDispatcher();
@@ -92,6 +96,7 @@ class Application extends App {
 		RoomShareProvider::register($dispatcher);
 		SignalingListener::register($dispatcher);
 		CommandListener::register($dispatcher);
+		ResourceListener::register($dispatcher);
 
 		$this->registerNavigationLink($server);
 		$this->registerRoomActivityHooks($dispatcher);
@@ -111,6 +116,12 @@ class Application extends App {
 				'name' => $l->t('Talk'),
 			];
 		});
+	}
+
+	protected function registerCollaborationResourceProvider(IServerContainer $server): void {
+		/** @var IResourceManager $resourceManager */
+		$resourceManager = $server->query(IResourceManager::class);
+		$resourceManager->registerResourceProvider(ConversationProvider::class);
 	}
 
 	protected function registerClientLinks(IServerContainer $server): void {
